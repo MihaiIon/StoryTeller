ad693aaf989dc07a49598075d7b19824a7b9463e
Mathieu <mathieu.brousseau@hotmail.com>
2017-03-09 13:11:43 -0500

    Mathieu et Mihai : Terminé représentation Java de la BD.

--- app/src/main/java/app/storyteller/database/SQLRequests.java
+++ app/src/main/java/app/storyteller/database/SQLRequests.java
@@ -9,8 +9,11 @@ public class SQLRequests {
      *
      */
     public static final String CREATE_ALL_TABLES =
-            Database.ProfileTable.getTableCreationStatement()+
-            Database.Lala.getTableCreationStatement();
+            Database.ProfilesTable.getTableCreationStatement()
+            + Database.StoriesTable.getTableCreationStatement()
+            + Database.FavoritesTable.getTableCreationStatement()
+            + Database.CollaboratorsTable.getTableCreationStatement()
+            + Database.SentencesTable.getTableCreationStatement();
 
     public static final String DELETE_ALL_TABLES = "";
 }
