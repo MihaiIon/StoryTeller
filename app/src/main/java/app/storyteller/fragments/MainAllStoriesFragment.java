4579dbe23d12ae79db5a59690bf00bab1b9f957e
Vincent <vkd-2000@hotmail.com>
2017-03-27 21:18:58 -0400

    Navigator change color on selection

--- app/src/main/java/app/storyteller/fragments/MainAllStoriesFragment.java
+++ app/src/main/java/app/storyteller/fragments/MainAllStoriesFragment.java
@@ -5,8 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import app.storyteller.R;
 
@@ -17,48 +18,66 @@ import app.storyteller.R;
 public class MainAllStoriesFragment extends Fragment {
     ListView lv;
     String[] items;
+
+    LinearLayout all_stories;
+    LinearLayout my_stories;
+    LinearLayout favs_stories;
+    LinearLayout navigator[];
+
     /**
+     * @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+     * ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_all_stories, container, false);
+     * <p>
+     * return rootView;
+     * }
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_all_stories, container, false);
 
-        return rootView;
-    }
-    */
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        items = new String[] {"a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a",
-                "a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"};
+        items = new String[]{"a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a",
+                "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a"};
 
         View view = inflater.inflate(R.layout.fragment_all_stories, container, false);
         lv = (ListView) view.findViewById(R.id.listview);
         /*
         StoriesListAdapter sladapter = new StoriesListAdapter(this.getActivity(), items);
         lv.setAdapter(sladapter);*/
-        new Thread(new Runnable(){
+        new Thread(new Runnable() {
 
             @Override
             public void run() {
-                StoriesListAdapter adapter = new StoriesListAdapter(getActivity(),items);
+                StoriesListAdapter adapter = new StoriesListAdapter(getActivity(), items);
                 lv.setAdapter(adapter);
 
             }
         }).start();
-
-
 
         return view;
 
 
     }
 
+    // 0 : AllStorie    1: MyStories   2: FavsStories
+    public void navigatorToSelector(int Selector) {
+        navigatorResetColor();
+        navigator[Selector].setBackgroundColor(getResources().getColor(R.color.primary));
+        TextView v = (TextView) navigator[Selector].getChildAt(0);
+        v.setTextColor(getResources().getColor(R.color.softGrey));
 
-
+    }
+    // Remet couleur de base
+    public void navigatorResetColor(){
+        for(int i=0 ; i < navigator.length ; i++) {
+            navigator[i].setBackgroundColor(getResources().getColor(R.color.softGrey));
+            TextView v = (TextView) navigator[i].getChildAt(0);
+            v.setTextColor(getResources().getColor(R.color.primary));
+        }
+    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
         //Mettre action de back sur la fleche
         getActivity().findViewById(R.id.story_chooser_back_btn).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,36 +86,43 @@ public class MainAllStoriesFragment extends Fragment {
             }
         });
 
+        //On va get chacun des elements du navigator
+        all_stories = (LinearLayout) getActivity().findViewById(R.id.all_story_page);
+        my_stories = (LinearLayout) getActivity().findViewById(R.id.my_story_page);
+        favs_stories = (LinearLayout) getActivity().findViewById(R.id.favs_story_page);
 
+        //Tableau des LinearLayout(Navigator)
+        navigator = new LinearLayout[]{all_stories, my_stories, favs_stories};
 
-        getActivity().findViewById(R.id.all_story_page).setOnClickListener(new View.OnClickListener() {
+        //Set les actions des options du navigators
+        all_stories.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(),"All Stories",Toast.LENGTH_SHORT).show();
-            }
-        });
-        getActivity().findViewById(R.id.my_story_page).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(),"My Stories",Toast.LENGTH_SHORT).show();
-            }
-        });
-        getActivity().findViewById(R.id.favs_story_page).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(),"Favs Stories",Toast.LENGTH_SHORT).show();
+                navigatorToSelector(0);
+                //Thread qui remplace le listView
             }
         });
 
+
+        my_stories.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigatorToSelector(1);
+                //Thread qui remplace le listView
+            }
+        });
+
+
+        favs_stories.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigatorToSelector(2);
+                //Thread qui remplace le listView
+            }
+        });
+        //Set a All Stories au depart
+        navigatorToSelector(0);
 
     }
 
 }
-
-/*
-* Tentative faites pour show/hide action bar
-* 1. OnResume (Fragment) - change rien
-* 2. OnViewCreated (Fragment) - app crash
-* 3. Switch Case getItem (MainAcvitity) - Fonctionne a moitier
-* 4. OnCreate (Fragment) - app crash
-* */
