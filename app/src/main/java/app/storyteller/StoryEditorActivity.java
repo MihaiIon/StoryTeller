3c43844698da5a43f7532b639761c8c84a3dd0d0
Mihai <mihai.t.ionescu@gmail.com>
2017-03-29 20:00:41 -0400

    - Refactored the names of multiple files. - Added visual for StoryChooser Activity (incomplete) - Added StoryCreatorActivity + related fragments

--- app/src/main/java/app/storyteller/StoryEditorActivity.java
+++ app/src/main/java/app/storyteller/StoryEditorActivity.java
@@ -1,26 +1,17 @@
 package app.storyteller;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 /**
- * Created by Mihai on 2017-03-26.
+ * Created by Mihai on 2017-03-29.
  */
 
-public class StoryCreatorActivity extends AppCompatActivity {
+public class StoryEditorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_story_creator);
-
-
     }
-
-
 }
--- app/src/main/java/app/storyteller/fragments/SCreatorFragment1.java
+++ app/src/main/java/app/storyteller/fragments/SCreatorFragment1.java
@@ -3,6 +3,9 @@ package app.storyteller.fragments;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Mihai on 2017-03-26.
@@ -10,8 +13,9 @@ import android.support.annotation.Nullable;
 
 public class SCreatorFragment1 extends Fragment {
 
+    @Nullable
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
