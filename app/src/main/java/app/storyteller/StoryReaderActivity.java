a736cb39ef83234114c38a0fa5a16170045c9f1a
Mihai <mihai.t.ionescu@gmail.com>
2017-04-26 22:11:02 -0400

    Widened the textview in StoryReader.

--- app/src/main/java/app/storyteller/StoryReaderActivity.java
+++ app/src/main/java/app/storyteller/StoryReaderActivity.java
@@ -170,7 +170,7 @@ public class StoryReaderActivity extends AppCompatActivity {
      */
     private Spannable getFormattedStoryContent(String raw){
 
-        Spannable s = new SpannableString(raw);
+        Spannable s = new SpannableString(raw.substring(0,1)+"  "+raw.substring(1));
         s.setSpan(new RelativeSizeSpan(4f), 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         s.setSpan(new LineHeightSpan() {
             @Override
